ee50b7d6bba6c02a8db48588515cfa324154fd58
adilalimgozha <alimgozha2003@gmail.com>
2024-10-18 09:28:27 +0500

    blogs project

--- blog_application/blog_application/urls.py
+++ blog_application/blog_application/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('', include('users.urls')),
+    path('', include('relation.urls')),
+    path('', auth_views.LoginView.as_view(template_name='login.html'))
 ]
